792ca284edff91120c3576d37cbc1288a6fab27a
Jerson Janke <jersonjanke@gmail.com>
2026-04-14 15:01:18 -0300

    feat: adicionar campo imagemFiltro no Destino

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -481,6 +481,7 @@ export interface ApiDestinoDestino extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    imagemFiltro: Schema.Attribute.Media<'images'>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
